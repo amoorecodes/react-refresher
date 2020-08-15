2ec8e28e7bff524e575270d7353ead795ec7415a
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2020-08-14 17:25:49 -0700

    [add] Basic form for pet finder

--- src/hooks/SearchParams.js
+++ src/hooks/SearchParams.js
@@ -1,19 +1,61 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           location
-          <input id="location" value={location} placeholder="Location" onChange={e => setLocation(e.target.value)} />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
           <button>Submit</button>
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+            disabled={!breeds.length}
+          >
+            <option>ALL</option>
+            {breeds.map((breedFromList) => (
+              <option value={breedFromList} key={breedFromList}>
+                {breedFromList}
+              </option>
+            ))}
+          </select>
         </label>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SearchParams
+export default SearchParams;
