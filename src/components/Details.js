601080e739eb93307525ab0f795b18ee4446c160
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2020-08-15 18:33:17 -0700

    [add] Modals, finish course

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "../ThemeContext";
+import { navigate } from "@reach/router";
+import Modal from "../hooks/Modal";
 
 class Details extends Component {
   constructor(props) {
@@ -9,12 +12,15 @@ class Details extends Component {
 
     this.state = {
       loading: true,
+      showModal: false,
     };
   }
 
   async componentDidMount() {
     await pet.animal(this.props.id).then(({ animal }) => {
+      // console.log("animal: ", animal);
       this.setState({
+        url: animal.url,
         name: animal.name,
         animal: animal.type,
         breed: animal.breeds.primary,
@@ -26,21 +32,55 @@ class Details extends Component {
     }, console.error);
   }
 
+  toggleModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+  };
+
+  adopt = () => navigate(this.state.url);
+
   render() {
     if (this.state.loading) {
-      throw new Error("render error");
+      // throw new Error("render error");
       return <h1>Loading...</h1>;
     }
 
-    const { animal, breed, location, description, name, media } = this.state;
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      name,
+      media,
+      showModal,
+    } = this.state;
     return (
       <div className="details">
         <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {(themeHook) => (
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: themeHook[0] }}
+              >
+                Adopt {name}
+              </button>
+            )}
+          </ThemeContext.Consumer>
           <p>${description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <button onClick={this.adopt}>YES</button>
+                  <button onClick={this.toggleModal}>NO</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -10,7 +10,7 @@ class ErrorBoundary extends Component {
       redirect: false,
     };
   }
-  static getDerivedStateFromError(error) {
+  static getDerivedStateFromError() {
     return { hasError: true };
   }
 
